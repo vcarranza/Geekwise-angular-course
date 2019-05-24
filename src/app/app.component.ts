9b86294ebb01e75319e85155e3e66da15dedc2b0
vcarranza <viccarranza67@gmail.com>
2019-05-23 18:12:15 -0700

    first day of angular class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'geekwise-angular-course';
+  title:'Hello World!';
+  age: 90000;
+  isShown: "I am being shown";
+  notShown: "i am not being shown";
+
+
+  person: object = {
+    name: "Victor", 
+    age: 5000,
+    course: "angular"
+  };
+  people: object[]= [ 
+    {
+      name:"Victor",
+      age: 100000,
+      course:"angular"
+
+    }
+  ];
+
+  colors: string[] = ["red", "blue","green", "yellow" ];
+
+  myFunction(shouldShow: boolean): string {
+    // if (shouldShow) {return "i am being shown"} else {return "i am not being shown"}
+    return shouldShow ? this.isShown: this.notShown;
+  }
 }
